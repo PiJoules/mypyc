752bcc15eccf4b9da93141d47851d397ed6406fc
Leonard Chan <lc599@tux64-13.cs.drexel.edu>
2016-08-08 21:10:41 -0400

    create file object

--- Makefile
+++ Makefile
@@ -15,9 +15,14 @@ INCLUDES = -I$(INCLUDE_DIR)
 LIBS = -L$(LIB_DIR)
 override CFLAGS += -std=$(CSTANDARD) -Wall $(INCLUDES)
 
+TEST_FILE ?= samples/hello_world.pc
+
 %.o: %.c
 	$(CC) -c $(CFLAGS) -o $@ $<
 
 all:
 	$(CC) $(CFLAGS) -o $(OUTPUT) $(EXE) $(OBJECTS)
+
+valgrind:
+	valgrind --leak-check=yes ./$(OUTPUT) $(TEST_FILE)
 
--- pc.c
+++ pc.c
@@ -1,6 +1,50 @@
 #include "pc.h"
 
 
+/**
+ * File object stuff
+ */
+typedef struct FileIterator FileIterator;
+typedef struct File File;
+
+struct FileIterator {
+    FileIterator* next_iter;
+    char* line;
+};
+
+struct File {
+    FileIterator* iterator;
+    char* filename;
+    FILE* file_pointer;
+};
+
+File* open(char* filename){
+    File* file_obj = (File*)malloc(sizeof(File));
+    if (!file_obj){
+        fprintf(stderr, "Could not malloc %zu bytes for File object.\n", sizeof(File));
+        exit(EXIT_FAILURE);
+    }
+
+    // Set file pointer
+    FILE* fp = fopen(filename, "r");
+    if (!fp){
+        fprintf(stderr, "Null file pointer when opening file \"%s\".\n", filename);
+        exit(EXIT_FAILURE);
+    }
+    file_obj->file_pointer = fp;
+    file_obj->filename = filename;
+
+    return file_obj;
+}
+
+void File_close(File* file){
+    fclose(file->file_pointer);
+    free(file);
+}
+/* End of File object stuff */
+
+
+
 int main(int argc, char* argv[]){
     // Must provide filename
     if (argc < 2){
@@ -10,12 +54,14 @@ int main(int argc, char* argv[]){
 
     // Open file
     char* filename = argv[1];
-    FILE* fp = fopen(filename, "r");
-    if (!fp){
-        fprintf(stderr, "Null file pointer when opening file \"%s\".\n", filename);
-        exit(EXIT_FAILURE);
-    }
-    fclose(fp);
+    File* file = open(filename);
+
+    // Read lines
+    //ssize_t read_len
+    //while ((read))
+
+    // Close file
+    File_close(file);
 
     // Create list of words
 
